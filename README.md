58e335b5808068665b0b7a78e7af72e4db4516dc
kd112 <kd_112@protonmail.com>
2023-09-17 23:12:48 -0400

    feat: :construction: html pages, docs, template controller
    Added jupyter notebooks to docs for troubleshooting, added template_controller updates to manager rolling around to each page to keep from cluttering up the heavy lifting of the other controller functions
    
    API no longer works for MGL, big sad.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # watch-commander
 Lightweight WebApp to lookup mass general laws, post shift notes.
+
+# To get started:
+
+Clone the repo:
+https://github.com/kd1882/watch-commander.git
+
+Get Python Poetry:
+https://python-poetry.org/docs/
+
+Installing Poetry:
+Linux, macOS, Windows (WSL)
+```
+curl -sSL https://install.python-poetry.org | python3 -
+```
+
+Windows (Powershell)
+```
+(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -
+```
+
+Ensure you add poetry to your path. Once that is all done, cd to the watch commander directory and:
+```
+poetry install
+```
+And all the dependencies will be installed.
+
+You can then run the script via 
+```
+poetry run python3 server.py
+```
+
+or going into the shell
+```
+poetry shell
+
+python3 server.py
+```
--- flask_app/controllers/template_controller.py
+++ flask_app/controllers/template_controller.py
@@ -19,6 +19,95 @@ def dashboard():
     else:
         user = User.get_by_id(session['user_id'])
         return render_template('dashboard.html', user = user)
+    
+# getting started page
+@app.route('/getting_started')
+def getting_started():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        return render_template('getting_started.html')
+
+# mgl_reference page
+@app.route('/mgl_reference')
+def mgl_reference():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        user = User.get_by_id(session['user_id'])
+        return render_template('mgl_reference.html', user = user)
+
+# role call notes page
+@app.route('/role_call_notes')
+def role_call_notes():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        user = User.get_by_id(session['user_id'])
+        return render_template('rolecall_notes.html', user = user)
+
+# dept info page
+@app.route('/dept_info')
+def dept_info():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        user = User.get_by_id(session['user_id'])
+        return render_template('dept_information.html', user = user)
+
+# legal page
+@app.route('/legal')
+def legal():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        user = User.get_by_id(session['user_id'])
+        return render_template('legal.html', user = user)
+
+# about page
+@app.route('/about')
+def about():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        user = User.get_by_id(session['user_id'])
+        return render_template('about.html', user = user)
+
+# faq page
+@app.route('/faq')
+def faq():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        user = User.get_by_id(session['user_id'])
+        return render_template('faq.html', user = user)
+
+# contact the devs page
+@app.route('/contact')
+def contact():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        user = User.get_by_id(session['user_id'])
+        return render_template('contact.html', user = user)
+
+# account management page
+@app.route('/account_management')
+def account_management():
+    if not session.get('user_id'):
+        flash("Please login to access the application")
+        return redirect('/')
+    else:
+        user = User.get_by_id(session['user_id'])
+        return render_template('manage_account.html', user = user)
 
 #  Logout success
 @app.route('/logout/success')
